88c988ab86913bd56c0e6721c6f57ea3a541a9cf
shaula-hub <dagetil04@gmail.com>
2025-03-21 12:29:27 +0300

    Enhancing colors

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    hmr: {
-      overlay: false,
-    },
-  },
+  base: "/crazy-colors/",
 });
